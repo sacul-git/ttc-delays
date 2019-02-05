43029428ff0b95f487cee3d87efb36eddcfbb7bd
Lucas Goldstone <lu.goldstone@gmail.com>
2019-02-04 20:25:09 -0500

    Add skeleton of shiny app

---
+++ ttcmap.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(leaflet)
+library(tidyverse)
+
+# load map data
+m <- readRDS("data/busroutes.rds")
+# load delay data
+d <- read_csv("data/delay_data.csv") 
+
+# get incidents per year per route
+inc_per_route <- d %>%
+  group_by(Route) %>%
+  summarise(n_incidents = n(),
+            perc_incidents = (n() / nrow(d)) * 100
+            ) %>%
+  arrange(desc(n_incidents))
+
+Data <- inner_join(x = m, y = inc_per_route, by = c("route" = "Route"))
+
+leaflet(Data) %>% 
+    addPolylines(data = Data$geometry)
+
+
+ui <- bootstrapPage(
+  leafletOutput("map", width = "80%", height = "800"),
+  absolutePanel(top = 10, right = 10,
+    selectInput(inputId = "route", label = "Route",
+                choices = c(52)
+    ),
+    selectInput(inputId = "year", label = "Year",
+                choices = c(2014:2018))
+    )
+)
+
+server <- function(input, output, session){
+  filtered <- reactive({
+    Data %>% 
+        filter(route == input$route)
+  })
+  pal <- colorNumeric(
+    palette = "Reds",  
+    domain = Data$n_incidents)
+  # Static part of map:
+  output$map <- renderLeaflet({
+    leaflet(m) %>%
+      setView(lng = -79.367494, lat = 43.722780, zoom = 12) %>%
+      addProviderTiles(providers$Stamen.Toner)
+  })
+  observe({
+    pal <- pal
+    leafletProxy("map", data = filtered()) %>% 
+        addPolylines(data = filtered()$geometry, label = ~as.factor(route),
+                     fillOpacity = 1, color = ~pal(n_incidents))
+  })
+}
+
+shinyApp(ui, server)
