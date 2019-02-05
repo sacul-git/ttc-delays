ffdd296f51b7ba7061d12c4e982d6ce46e8200bd
Lucas Goldstone <lu.goldstone@gmail.com>
2019-02-05 18:21:40 -0500

    Change from bootstrap to fluidpage; Change aesthetics

--- ttcmap.R
+++ ttcmap.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyWidgets)
 library(leaflet)
 library(tidyverse)
 library(viridis)
@@ -24,55 +25,75 @@ ui <- shiny::fluidPage(
   shiny::titlePanel("TTC Delays"),
   shiny::mainPanel(
     leaflet::leafletOutput("map", width = "100%", height = "800")),
-  #shiny::absolutePanel(
-   shiny::sidebarPanel(
-     shiny::checkboxGroupInput(inputId = "route", label = "Routes",
-                               choices = sort(intersect(m$route, d$Route)),
-                               selected = c(52, 82),
-                               inline = TRUE,
-                               width = "100%"
+  shiny::sidebarPanel(
+    shinyWidgets::pickerInput(inputId = "route", label = "Routes",
+                              choices = sort(intersect(m$route, d$Route)),
+                              selected = c(52, 82),
+                              multiple = TRUE,
+                              options = list(
+                                `actions-box` = TRUE,
+                                `select-all-text` = "Select All Routes (slow render)",
+                                 `deselect-all-text` = "Deselect All Routes"
+                                            )
     ),
     shiny::selectInput(inputId = "year", label = "Year",
                 choices = c(2014:2018))
-    #shiny::dateRangeInput(inputId = "daterange", label = "Dates",
-                          #start = min(d$Report_Date),
-                          #end = max(d$Report_Date),
-                          #min = min(d$Report_Date),
-                          #max = max(d$Report_Date))
+
+    #shiny::plotOutput(outputId = "delayplot")
+
     )
 )
 
+
 server <- function(input, output, session){
+  # Filter the data
   filtered <- reactive({
     Data %>%
-        filter(route %in% input$route & year == input$year)
+      filter(route %in% input$route & year == input$year)
   })
-
+  # Define color palette
   colorpal_legend <- shiny::reactive(
     leaflet::colorNumeric(
     palette = "viridis",
     domain = Data$n_incidents))
-
-  # Static part of map:
+  # Static map:
   output$map <- leaflet::renderLeaflet({
     leaflet(Data) %>%
       leaflet::setView(lng = -79.45, lat = 43.7, zoom = 12) %>%
       leaflet::addProviderTiles(providers$Stamen.TonerLite) %>%
       leaflet::addLegend(position = "topright", pal = colorpal_legend(),
-                values = Data$n_incidents,
-                title = "Number of incidents on each route")
+                         values = Data$n_incidents,
+                         title = "Reported Delays")
   })
+  # Dynamic map:
   observe({
     pal <- colorpal_legend()
-
     leaflet::leafletProxy("map", data = filtered()) %>%
-        leaflet::clearShapes() %>%
-        leaflet::addPolylines(label = ~paste(as.factor(n_incidents),
-                                             " incidents reported on route ",
-                                             as.factor(route),
-                                             " in ", as.factor(year)),
-                     color = ~pal(filtered()$n_incidents))
+      leaflet::clearShapes() %>%
+      leaflet::addPolylines(label = ~paste("Route ",
+                                           as.factor(route)),
+                            labelOptions = labelOptions(textsize = "25px"),
+                            highlightOptions = highlightOptions(bringToFront = TRUE,
+                                                                weight = 8),
+                            color = ~pal(filtered()$n_incidents), opacity = 1,
+                            popup = paste("<h3>Route ", filtered()$route,
+                            ":</h3><br><h4>", filtered()$n_incidents, 
+                            " Delays Reported in ", filtered()$year, "</h4>")
+                            )
   })
+
+  #observeEvent(input$map_shape_click, {
+  #p <- input$map_shape_click
+  #print(p)
+  #})
+
+  #output$delayplot <- renderPlot({
+      #d <- Data
+      #ggplot(data = d, aes(x = year, y = n_incidents)) +
+          #geom_bar(stat = "identity")
+  #})
+
 }
 
 shiny::shinyApp(ui, server)
+
