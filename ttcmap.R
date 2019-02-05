d4c41778cf888822a606c2e97ac60c7abe314194
Lucas Goldstone <lu.goldstone@gmail.com>
2019-02-05 13:06:14 -0500

    Show delays by year and route on map

--- ttcmap.R
+++ ttcmap.R
@@ -2,59 +2,77 @@ library(shiny)
 library(leaflet)
 library(tidyverse)
 library(viridis)
+# sf package *must* be loaded in order to merge 
+# route and delay data and still get sf object
+library(sf)
 
 # load map data
 m <- readRDS("data/busroutes.rds")
 # load delay data
-d <- read_csv("data/delay_data.csv") 
+d <- read_csv("data/delay_data.csv")
 
 # get incidents per year per route
-inc_per_route <- d %>%
-  group_by(Route) %>%
-  summarise(n_incidents = n(),
-            perc_incidents = (n() / nrow(d)) * 100
-            ) %>%
-  arrange(desc(n_incidents))
+inc_year_route <- d %>%
+    mutate(year = format(.$Report_Date, "%Y")) %>%
+    group_by(Route, year) %>%
+    summarise(n_incidents = n()) %>%
+    arrange(desc(n_incidents))
 
-Data <- inner_join(x = m, y = inc_per_route, by = c("route" = "Route"))
+Data <- inner_join(x = m, y = inc_year_route, by = c("route" = "Route"))
 
-sort(unique(Data$route))
-
-ui <- bootstrapPage(
-  leafletOutput("map", width = "80%", height = "800"),
-  absolutePanel(top = 10, right = 10,
-    selectInput(inputId = "route", label = "Route",
-                choices = sort(unique(Data$route))
+ui <- shiny::fluidPage(
+  shiny::titlePanel("TTC Delays"),
+  shiny::mainPanel(
+    leaflet::leafletOutput("map", width = "100%", height = "800")),
+  #shiny::absolutePanel(
+   shiny::sidebarPanel(
+     shiny::checkboxGroupInput(inputId = "route", label = "Routes",
+                               choices = sort(intersect(m$route, d$Route)),
+                               selected = c(52, 82),
+                               inline = TRUE,
+                               width = "100%"
     ),
-    selectInput(inputId = "year", label = "Year",
+    shiny::selectInput(inputId = "year", label = "Year",
                 choices = c(2014:2018))
+    #shiny::dateRangeInput(inputId = "daterange", label = "Dates",
+                          #start = min(d$Report_Date),
+                          #end = max(d$Report_Date),
+                          #min = min(d$Report_Date),
+                          #max = max(d$Report_Date))
     )
 )
 
 server <- function(input, output, session){
   filtered <- reactive({
-    Data %>% 
-        filter(route == input$route)
+    Data %>%
+        filter(route %in% input$route & year == input$year)
   })
 
-  colorpal <- reactive(colorNumeric(
+  colorpal_legend <- shiny::reactive(
+    leaflet::colorNumeric(
     palette = "viridis",
     domain = Data$n_incidents))
 
   # Static part of map:
-  output$map <- renderLeaflet({
+  output$map <- leaflet::renderLeaflet({
     leaflet(Data) %>%
-      setView(lng = -79.367494, lat = 43.722780, zoom = 12) %>%
-      addProviderTiles(providers$Stamen.Toner) %>% 
-      addLegend(position = "topright", pal = colorpal(), values = Data$n_incidents, title = "Number of incidents on each route")
+      leaflet::setView(lng = -79.45, lat = 43.7, zoom = 12) %>%
+      leaflet::addProviderTiles(providers$Stamen.TonerLite) %>%
+      leaflet::addLegend(position = "topright", pal = colorpal_legend(),
+                values = Data$n_incidents,
+                title = "Number of incidents on each route")
   })
   observe({
-    pal <- colorpal()
+    pal <- colorpal_legend()
 
-    leafletProxy("map", data = filtered()) %>% 
-        clearShapes() %>% 
-        addPolylines(label = ~as.factor(route), color = ~pal(n_incidents))
+    leaflet::leafletProxy("map", data = filtered()) %>%
+        leaflet::clearShapes() %>%
+        leaflet::addPolylines(label = ~paste(as.factor(n_incidents),
+                                             " incidents reported on route ",
+                                             as.factor(route),
+                                             " in ", as.factor(year)),
+                     color = ~pal(filtered()$n_incidents))
   })
 }
 
-shinyApp(ui, server)
+shiny::shinyApp(ui, server)
