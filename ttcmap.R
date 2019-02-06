fa22d246730f7ddb5fd2f434e91569eb8e790e97
Lucas Goldstone <lu.goldstone@gmail.com>
2019-02-05 23:15:07 -0500

    Add plots of route through years

--- ttcmap.R
+++ ttcmap.R
@@ -11,39 +11,34 @@ library(sf)
 m <- readRDS("data/busroutes.rds")
 # load delay data
 d <- read_csv("data/delay_data.csv")
-
 # get incidents per year per route
-inc_year_route <- d %>%
+Data <- inc_year_route <- d %>%
     mutate(year = format(.$Report_Date, "%Y")) %>%
     group_by(Route, year) %>%
     summarise(n_incidents = n()) %>%
-    arrange(desc(n_incidents))
-
-Data <- inner_join(x = m, y = inc_year_route, by = c("route" = "Route"))
+    arrange(desc(n_incidents)) %>%
+    inner_join(x = m, y = ., by=c("route" = "Route"))
 
 ui <- shiny::fluidPage(
   shiny::titlePanel("TTC Delays"),
   shiny::mainPanel(
     leaflet::leafletOutput("map", width = "100%", height = "800")),
-  shiny::sidebarPanel(
-    shinyWidgets::pickerInput(inputId = "route", label = "Routes",
-                              choices = sort(intersect(m$route, d$Route)),
-                              selected = c(52, 82),
-                              multiple = TRUE,
-                              options = list(
-                                `actions-box` = TRUE,
-                                `select-all-text` = "Select All Routes (slow render)",
-                                 `deselect-all-text` = "Deselect All Routes"
-                                            )
+    shiny::sidebarPanel(
+      shinyWidgets::pickerInput(inputId = "route", label = "Routes",
+                                choices = sort(unique(Data$route)),
+                                selected = c(35, 36, 52),
+                                multiple = TRUE,
+                                options = list(
+                                  `actions-box` = TRUE,
+                                  `select-all-text` = "Select All Routes (slow render)",
+                                  `deselect-all-text` = "Deselect All Routes"
+                                              )
     ),
-    shiny::selectInput(inputId = "year", label = "Year",
-                choices = c(2014:2018))
-
-    #shiny::plotOutput(outputId = "delayplot")
-
+      shiny::selectInput(inputId = "year", label = "Year",
+                  choices = c(2014:2018)),
+      shiny::plotOutput(outputId = "plot")
     )
 )
-
 
 server <- function(input, output, session){
   # Filter the data
@@ -61,39 +56,43 @@ server <- function(input, output, session){
     leaflet(Data) %>%
       leaflet::setView(lng = -79.45, lat = 43.7, zoom = 12) %>%
       leaflet::addProviderTiles(providers$Stamen.TonerLite) %>%
-      leaflet::addLegend(position = "topright", pal = colorpal_legend(),
+      leaflet::addLegend(position = "topright", opacity = 1, 
+                         pal = colorpal_legend(),
                          values = Data$n_incidents,
                          title = "Reported Delays")
   })
-  # Dynamic map:
+  # Load selected Bus Routes:
   observe({
     pal <- colorpal_legend()
     leaflet::leafletProxy("map", data = filtered()) %>%
       leaflet::clearShapes() %>%
-      leaflet::addPolylines(label = ~paste("Route ",
+      leaflet::addPolylines(layerId = filtered()$route, label = ~paste("Route ",
                                            as.factor(route)),
                             labelOptions = labelOptions(textsize = "25px"),
                             highlightOptions = highlightOptions(bringToFront = TRUE,
                                                                 weight = 8),
                             color = ~pal(filtered()$n_incidents), opacity = 1,
-                            popup = paste("<h3>Route ", filtered()$route,
-                            ":</h3><br><h4>", filtered()$n_incidents, 
-                            " Delays Reported in ", filtered()$year, "</h4>")
+                            popup = str_glue("<h3>Route {filtered()$route}:</h3>",
+                                             "<br><h4>{filtered()$n_incidents} ",
+                                             "Delays Reported in {filtered()$year} </h4>")
                             )
   })
-
-  #observeEvent(input$map_shape_click, {
-  #p <- input$map_shape_click
-  #print(p)
-  #})
-
-  #output$delayplot <- renderPlot({
-      #d <- Data
-      #ggplot(data = d, aes(x = year, y = n_incidents)) +
-          #geom_bar(stat = "identity")
-  #})
-
+  # Bar graph of the delays on selected route per year
+  observeEvent(input$map_shape_click, {
+  r <- input$map_shape_click$id
+  d  <- Data %>% filter(route == r) %>% distinct(year, .keep_all=TRUE)
+  output$plot <- renderPlot({
+    ggplot(data = d, aes(x = year, y = n_incidents)) +
+        geom_bar(stat = "identity", aes(fill = n_incidents)) +
+        scale_fill_viridis(limits = c(min(Data$n_incidents), max(Data$n_incidents))) +
+        labs(title = str_glue("Delays per Year on Route {r}"),
+             x = "Year", y = "Number of Reported Delays") +
+        theme(legend.position = "none", 
+              plot.title = element_text(hjust = 0.5, size = 25),
+              axis.title = element_text(size = 18),
+              axis.text = element_text(size = 15))
+    })
+  })
 }
 
 shiny::shinyApp(ui, server)
-
